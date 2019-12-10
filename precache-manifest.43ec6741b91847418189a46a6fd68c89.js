2375cd7bd9483514a84342516b55b603d2582e5c
diegocamy <53247032+diegocamy@users.noreply.github.com>
2019-12-09 22:58:59 -0300

    Updates

---
+++ precache-manifest.43ec6741b91847418189a46a6fd68c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a9236dde7894afd6b8025e7104bba6",
+    "url": "/dealer/index.html"
+  },
+  {
+    "revision": "858db02d32a484da1c74",
+    "url": "/dealer/static/css/main.c49e02bd.chunk.css"
+  },
+  {
+    "revision": "77620d30c1df13588094",
+    "url": "/dealer/static/js/2.d5411205.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dealer/static/js/2.d5411205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "858db02d32a484da1c74",
+    "url": "/dealer/static/js/main.5370577f.chunk.js"
+  },
+  {
+    "revision": "96feb5f8a62604ecb8cb",
+    "url": "/dealer/static/js/runtime-main.cde2ee44.js"
+  }
+]);
